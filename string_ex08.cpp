ac2fc99e7c376ddceaf947e44b156b27d07a807c
nmi21 <nicholas.iturraran@gmail.com>
2023-08-08 11:34:39 -0700

    remove leading zeros

---
+++ string_ex08.cpp
@@ -0,0 +1,36 @@
+/*
+Remove Leading Zeros From String in C++
+
+https://www.geeksforgeeks.org/remove-leading-zeros-from-string-in-cpp/
+*/
+
+#include <iostream>
+
+std::string removeLeadingZeros(std::string & str) {
+
+    // find out how many leading characters are either strings or spaces
+    int counter = 0;
+    while (str[counter] == '0') {
+        ++counter;
+    }
+
+    // remove the amount of characters counted
+    str.erase(0, counter);
+
+    return str;
+}
+
+int main() {
+
+    // collect string from user to modify
+    std::string str_in;
+    std::cout << "Remove leading 0s and spaces!" << std::endl 
+        << "Enter a number or set of words to remove:" << std::endl;
+    std::cin >> str_in;
+
+    // modify string and print result
+    std::cout << "Zeroes removed: " << std::endl
+        << removeLeadingZeros(str_in) << std::endl;
+
+    return 0;
+}
