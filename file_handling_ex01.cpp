3ff0b58c0f1aea549ae71e4731f1a146b376b994
nmi21 <nicholas.iturraran@gmail.com>
2023-08-09 10:34:51 -0700

    file handling: create a file

---
+++ file_handling_ex01.cpp
@@ -0,0 +1,32 @@
+/*
+C++ program to create a file
+
+https://www.geeksforgeeks.org/c-program-to-create-a-file/#
+*/
+
+#include <iostream>
+#include <fstream>
+
+void createFile(const std::string & file) {
+    
+    // create a file named 'file'
+    std::ofstream out_stream {file};
+
+    // verify it was created
+    if (!out_stream) {
+        throw std::runtime_error("Cannot open output file");
+        return;
+    }
+
+    std::cout << "File '" << file << "' successfully created" << std::endl;
+}
+
+
+int main() {
+
+    std::string file_name = "my_test_file.txt";
+
+    createFile(file_name);
+
+    return 0;
+}
