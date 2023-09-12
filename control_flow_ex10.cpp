f04518da184eaaed373cb645edd8e7c2fc541eeb
nmi21 <nicholas.iturraran@gmail.com>
2023-09-11 21:43:47 -0700

    added another method to be the same as Jennifer's version

--- control_flow_ex10.cpp
+++ control_flow_ex10.cpp
@@ -50,81 +50,104 @@ Method 2:
 - multiply out
 
 */
-// Create a sieve
-std::vector<bool> sieveOfEratosthenes(int size) {
-    // Return a vector of true/false where sieve[i] checks if i is prime
+// // Create a sieve
+// std::vector<bool> sieveOfEratosthenes(int size) {
+//     // Return a vector of true/false where sieve[i] checks if i is prime
     
-    std::vector<bool> sieve(size + 1, true);
-    sieve[0] = sieve[1] = false;
+//     std::vector<bool> sieve(size + 1, true);
+//     sieve[0] = sieve[1] = false;
 
-    for (int i = 2; i <= size; ++i) {
-        if (sieve[i]) {
-            for (int j = 2 * i; j <= size; j += i) {
-                sieve[j] = false;
-            }
-        }
-    }
+//     for (int i = 2; i <= size; ++i) {
+//         if (sieve[i]) {
+//             for (int j = 2 * i; j <= size; j += i) {
+//                 sieve[j] = false;
+//             }
+//         }
+//     }
 
-    return sieve;
-}
+//     return sieve;
+// }
 
-// Extract primes from the sieve
-std::vector<int> getPrimes(int limit) {
-    // Return all primes <= limit
+// // Extract primes from the sieve
+// std::vector<int> getPrimes(int limit) {
+//     // Return all primes <= limit
     
-    std::vector<bool> sieve = sieveOfEratosthenes(limit);
+//     std::vector<bool> sieve = sieveOfEratosthenes(limit);
     
-    std::vector<int> primes;
-    for (int i = 0; i < sieve.size(); ++i) {
-        if (sieve[i]) {
-            primes.push_back(i);
-        }
-    }
+//     std::vector<int> primes;
+//     for (int i = 0; i < sieve.size(); ++i) {
+//         if (sieve[i]) {
+//             primes.push_back(i);
+//         }
+//     }
 
-    return primes;
-}
+//     return primes;
+// }
 
-// Prime factor an integer given a vector of available primes
-std::vector<int> primeFactor(int n, const std::vector<int> & prime_numbers) {
-    // intialize factors with 0s
-    std::vector<int> factors(prime_numbers.size(), 0);
+// // Prime factor an integer given a vector of available primes
+// std::vector<int> primeFactor(int n, const std::vector<int> & prime_numbers) {
+//     // intialize factors with 0s
+//     std::vector<int> factors(prime_numbers.size(), 0);
 
-    for (int i = 0; i < prime_numbers.size(); ++i) {
-        while (n % prime_numbers[i] == 0) {
-            factors[i] += 1;
-            n /= prime_numbers[i];
-        }
-    }
+//     for (int i = 0; i < prime_numbers.size(); ++i) {
+//         while (n % prime_numbers[i] == 0) {
+//             factors[i] += 1;
+//             n /= prime_numbers[i];
+//         }
+//     }
 
-    return factors;
-}
+//     return factors;
+// }
 
-// Determine the LCM of a and b
+// // Determine the LCM of a and b
+// int lcm(int a, int b) {
+//     // get the prime numbers up to the larger of a and b
+//     std::vector<int> primes = getPrimes(std::max(a, b));
+
+//     // factor each number a and b based on the list of primes
+//     std::vector<int> factors_a = primeFactor(a, primes);
+//     std::vector<int> factors_b = primeFactor(b, primes);
+
+//     // determine the max of each of the factors and add to the vector
+//     std::vector<int> factors_max(primes.size());
+//     for (int i = 0; i < primes.size(); ++i) {
+//         factors_max[i] = std::max(factors_a[i], factors_b[i]);
+//     }
+
+//     // multiply the value of the prime by the max factor exponent
+//     int lcm = 1;
+//     for (int i = 0; i < primes.size(); ++i) {
+//         int prime = primes[i];
+//         int exp = factors_max[i];
+//         lcm *= std::pow(prime, exp);
+//     }
+
+//     return lcm;
+// }
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+// Method 3: A Simple Algorithm
 int lcm(int a, int b) {
-    // get the prime numbers up to the larger of a and b
-    std::vector<int> primes = getPrimes(std::max(a, b));
-
-    // factor each number a and b based on the list of primes
-    std::vector<int> factors_a = primeFactor(a, primes);
-    std::vector<int> factors_b = primeFactor(b, primes);
-
-    // determine the max of each of the factors and add to the vector
-    std::vector<int> factors_max(primes.size());
-    for (int i = 0; i < primes.size(); ++i) {
-        factors_max[i] = std::max(factors_a[i], factors_b[i]);
+    // This method compares a summation of a and a summation of b
+    // If the summation of a is < summation of b, then summation of a += a
+    // If the summation of b is < summation of a, then summation of b += b
+    // If the sums are equal, then we have found the LCM!
+    
+    int a_val = a;
+    int b_val = b;
+    while (a != b) {
+        if (a < b) {
+            a += a_val;
+        } else {
+            b += b_val;
+        }
     }
 
-    // multiply the value of the prime by the max factor exponent
-    int lcm = 1;
-    for (int i = 0; i < primes.size(); ++i) {
-        int prime = primes[i];
-        int exp = factors_max[i];
-        lcm *= std::pow(prime, exp);
-    }
-
-    return lcm;
+    return a;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 int main() {
@@ -136,6 +159,8 @@ int main() {
     // Calculate and print result
     std::cout << "LCM(" << a << ", " << b << ") == " 
         << lcm(a, b) << std::endl;
+
+    std::cout << a << std::endl;
 
     return 0;
 }
