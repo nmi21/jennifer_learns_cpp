c59110245c6d73f0519ee5c12c6c1ada98902a98
nmi21 <nicholas.iturraran@gmail.com>
2023-09-17 20:43:13 -0700

    added some comments and printed first several results

--- control_flow_ex17.cpp
+++ control_flow_ex17.cpp
@@ -8,10 +8,31 @@ https://www.geeksforgeeks.org/cpp-program-for-fibonacci-numbers/
 #include <system_error>
 
 int fibonacciNumber(int n) {
+    // Returns the nth Fibonacci number
+    // 
+    // The following is how the sequence starts:
+    //  fibonacciNumber(0) == 0
+    //  fibonacciNumber(1) == 1
+    //  fibonacciNumber(2) == 1
+    //  fibonacciNumber(3) == 2
+    //  fibonacciNumber(4) == 3
+    //  fibonacciNumber(5) == 5
+    //  fibonacciNumber(6) == 8
+    //  fibonacciNumber(7) == 13
+    //  fibonacciNumber(8) == 21
+    //  fibonacciNumber(9) == 34
+    //  fibonacciNumber(10) == 55
+    //  ...etc...
+
+    // note that inputs must be > 0
     if (n < 0) {
         std::runtime_error("cannot have fibonacci < 0");
     }
 
+    // handles the first handful of cases which are definitions of Fibonacci numbers
+    // namely:
+    //  F(1) = 1
+    //  F(2) = 1
     if (n == 0 || n == 1) {
         return n;
     }
@@ -20,6 +41,11 @@ int fibonacciNumber(int n) {
 }
 
 int main() {
+
+    // print some of the first Fibonacci numbers
+    for (int i = 0; i < 10 + 1; ++i) {
+        std::cout << "fibonacciNumber(" << i << ") == " << fibonacciNumber(i) << std::endl;
+    }
 
     // collect user input
     int num;
